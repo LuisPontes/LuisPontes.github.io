1e6a337fbbd0181e5e8cba19e8b2065a3de1d3ce
Luis Pontes <Luismarquespontes@gmail.com>
2020-05-10 17:54:09 +0200

    Updates

---
+++ precache-manifest.9e0e922c02fb8bf7dcb64df0dfa69f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2ccfc2c983452336260b029e5b1946",
+    "url": "/covid-info-app/index.html"
+  },
+  {
+    "revision": "408208f6d21517473caa",
+    "url": "/covid-info-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "cf964029c5b69d0db5f2",
+    "url": "/covid-info-app/static/css/main.2676e2d4.chunk.css"
+  },
+  {
+    "revision": "408208f6d21517473caa",
+    "url": "/covid-info-app/static/js/2.e4e7188b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-info-app/static/js/2.e4e7188b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf964029c5b69d0db5f2",
+    "url": "/covid-info-app/static/js/main.57baeafc.chunk.js"
+  },
+  {
+    "revision": "763130ce9525c6abb790",
+    "url": "/covid-info-app/static/js/runtime-main.48ed677e.js"
+  }
+]);
